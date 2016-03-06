a50240e9b270feb53dae6f42ecc24c27952f6d29
singhya <yash.minesweeper@gmail.com>
2016-03-05 20:14:43 -0800

    Clean up

---
+++ Mancala Game Playing Agent/testCase1.txt
@@ -0,0 +1,23 @@
+Test Case:
+2
+1
+5
+1
+3
+0
+0
+
+Next state
+0 
+0 
+0
+4
+
+Traversal:
+Node,Depth,Value
+root,0,-Infinity
+B2,1,Infinity
+B2,1,4
+root,0,4
+
+
--- Mancala Game Playing Agent/testLimits.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <limits>
-using namespace std;
-int main()
-{
-	cout<<numeric_limits<int>::min();
-	cout<<numeric_limits<int>::max();
-}
